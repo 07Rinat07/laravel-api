aef5374caa55000f95ecd1a6a08cba4518d1f469
Rinat Sarmuldin <ura07srr@gmail.com>
2024-10-18 11:05:48 +0500

    Token expiration date

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -1,17 +1,22 @@
 <?php
+
 namespace App\Http\Controllers\Api\V1;
+
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
+
     public function register(StoreUserRequest $request)
     {
         return User::create($request->all());
     }
+
     public function login(LoginUserRequest $request)
     {
         if (!Auth::attempt($request->only(['email', 'password']))) {
@@ -19,6 +24,7 @@ class AuthController extends Controller
                 'message' => 'Wrong email or password'
             ], 401);
         }
+
 //        $user = Auth::user();
         $user = User::query()->where('email', $request->email)->first();
         $user->tokens()->delete();
@@ -27,8 +33,13 @@ class AuthController extends Controller
             'token' => $user->createToken("Token of user: {$user->name}")->plainTextToken
         ]);
     }
+
     public function logout()
     {
-        return 'Logout';
+        Auth::user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Token removed'
+        ]);
     }
+
 }
